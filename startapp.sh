3a9592e407f286358091b6695457f77ee3cf5e2a
zocker-160 <zocker1600@posteo.net>
2021-06-14 15:37:28 +0200

    update logic for username and password

--- startapp.sh
+++ startapp.sh
@@ -9,18 +9,26 @@ curl https://www.sheepit-renderfarm.com/media/applet/client-latest.php -o /sheep
 
 
 #Autodetect cores
-if [ $cpu -eq 0 ]; then
-    echo "No thread count specified, autodetected `nproc` threads."
-    cpu=`nproc`
+if [ $cpu -eq 0 ]
+then
+	cpu = $(nproc)
+    echo "No thread count specified, autodetected $cpu threads."
 fi
 
 echo Starting client...
-if [[ $UI_MODE == "DARK" ]];
+if [ "$UI_MODE" == "DARK" ]
 then
 	echo "switching to dark mode"
 	# fix background color (I have no idea how to do that properly soo yeah)
 	sed -i -e 's/<body>/<body style="background-color: #292b2d">/g' /opt/novnc/index.html
-	java -jar /sheep/sheepit-client.jar -cores $cpu -cache-dir /tmp -login $USER_NAME -password $USER_PASSWORD -theme dark
+	theme = "dark"
+else
+	theme = "light"
 fi
 
-java -jar /sheep/sheepit-client.jar -cores $cpu -cache-dir /tmp -login $USER_NAME -password $USER_PASSWORD -theme light
+if [ -z $USER_NAME ] || [ -z $USER_PASSWORD ]
+then
+	java -jar /sheep/sheepit-client.jar -cores $cpu -cache-dir /tmp -$theme
+else
+	java -jar /sheep/sheepit-client.jar -cores $cpu -cache-dir /tmp -login $USER_NAME -password $USER_PASSWORD -theme $theme
+fi
